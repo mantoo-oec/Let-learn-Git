ed39fdbec30ea8c3f194665f82dfdfbc42bbadcf
mantoo-oec <mantoo.sinha@xperi.com>
2022-04-10 08:16:00 +0530

    adding proper code

--- add.c
+++ add.c
@@ -1,5 +1,14 @@
-This file is for addition
-
-
-
-Anding some more things
+#include<stdio.h>
+ 
+int main() {
+   int a, b, sum;
+ 
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b);
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", sum);
+ 
+   return(0);
+}
